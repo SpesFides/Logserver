ab98e03ec5f851c4f97787965aeb71c434f5638a
SpesFides <b.joosten@st.hanze.nl>
2017-06-29 18:15:15 +0200

    Update Pull_git.sh

--- Pull_git.sh
+++ Pull_git.sh
@@ -8,8 +8,8 @@ echo Timetogotoschool45
 git merge origin/master
 
 #activeer scrips
-echo Saltstack_master.sh
-echo Saltstack_minion_master.sh
-echo Monitor_Munin.sh
-echo Docker_master.sh
-echo Syslog.sh
+#echo Saltstack_master.sh
+#echo Saltstack_minion_master.sh
+#echo Monitor_Munin.sh
+#echo Docker_master.sh
+#echo Syslog.sh
